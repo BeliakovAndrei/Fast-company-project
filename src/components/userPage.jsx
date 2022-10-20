2a66953cb816c6611f11f4b85daa5f9a90646742
Beliakov Andrei <andrei_beliakov@163.com>
2022-10-20 19:23:28 +0800

    userPage and userNav are modified

--- src/components/userPage.jsx
+++ src/components/userPage.jsx
@@ -3,24 +3,27 @@ import { useHistory } from "react-router-dom";
 import API from "../API";
 import Qualitie from "./quality";
 
-const history = useHistory();
 const UserPage = () => {
-    const [user, setUser] = useState()
+    const history = useHistory();
+    const [user, setUser] = useState();
     const handleAllUsers = () => {
-        history.push("/users")
-};
+        history.push("/users");
+    };
     useEffect(() => {
-        API.users.fetchAll().then((data) => setUser(data));
+        API.users.getById().then((data) => setUser(data));
     }, []);
-    return user ? <div>
+    return user
+        ? <div>
             <h2>{user.name}</h2>
             <h3>{user.professions}</h3>
-            <h4>{user.qualities.map(qual=><Qualitie />)}</h4>
+            <h4>{user.qualities.map(qual => <Qualitie key={name} {...qual}/>)}</h4>
             <h4>{user.completedMeetings}</h4>
             <h3>{user.rate}</h3>
-            <button onClick={()=>{handleAllUsers()}}>Все пользователи</button>
+            <button onClick={() => { handleAllUsers(); }}>
+                Все пользователи
+            </button>
         </div>
-    : 'loading...'
+        : "loading...";
 };
 
 export default UserPage;
--- src/components/usersNav.jsx
+++ src/components/usersNav.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import Users from "./users";
+import UserPage from "./userPage";
+import { useParams } from "react-router-dom";
 
+const { userId } = useParams();
 const UsersNav = () => {
-    return userId ? <userPage/> : <Users/>;
+    return userId ? <UserPage /> : <Users />;
 };
 
 export default UsersNav;
