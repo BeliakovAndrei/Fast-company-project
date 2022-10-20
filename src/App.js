7c2e41c42150d3626e0ac5edfa1c502f261e2fde
Beliakov Andrei <andrei_beliakov@163.com>
2022-10-20 22:57:52 +0800

    bugs are fixed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import NavBar from "./components/navBar";
-import Users from "./components/users";
 import { Route, Switch } from "react-router-dom";
 import Login from "./components/login";
 import Main from "./components/main";
@@ -10,7 +9,6 @@ const App = () => {
     return <div>
         <NavBar />
         <Switch>
-            <Route path="/users/" component={Users} />
             <Route path="/login" component={Login} />
             <Route path="/users/:userId?" component={UsersNav} />
             <Route path="/" exact component={Main} />
